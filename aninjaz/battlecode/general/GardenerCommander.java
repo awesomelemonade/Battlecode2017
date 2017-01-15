c2085f84063039772c25dee6469312af295341ae
awesomelemonade <awesomelemonade@gmail.com>
2017-01-14 22:06:14 -0500

    Created Scout AI

--- aninjaz/battlecode/general/GardenerCommander.java
+++ aninjaz/battlecode/general/GardenerCommander.java
@@ -6,6 +6,8 @@ import battlecode.common.RobotController;
 import battlecode.common.RobotType;
 
 public class GardenerCommander {
+	private static int scoutSpawnRate = 1000;
+	private static boolean lowHealth = false;
 	public static void run(RobotController controller) throws GameActionException{
 		Direction direction = Util.randomDirection();
 		while(true){
@@ -18,25 +20,36 @@ public class GardenerCommander {
 			if(tries>0){
 				controller.move(direction);
 			}
-			if((controller.getHealth()/controller.getType().maxHealth)<Constants.LOW_HEALTH){ //If commander about to die :(
-				controller.broadcast(Constants.BROADCAST_REQUEST_ARCHON_GARDENER_COMMANDER,
-						controller.readBroadcast(Constants.BROADCAST_REQUEST_ARCHON_GARDENER_COMMANDER)+1);
+			if(!lowHealth){
+				if((controller.getHealth()/controller.getType().maxHealth)<Constants.LOW_HEALTH){ //If commander about to die :(
+					controller.broadcast(Constants.BROADCAST_REQUEST_ARCHON_GARDENER_COMMANDER,
+							controller.readBroadcast(Constants.BROADCAST_REQUEST_ARCHON_GARDENER_COMMANDER)+1);
+				}
+				lowHealth = true;
 			}
 			hireScout(controller);
 			Util.yieldByteCodes();
 		}
 	}
 	public static void hireScout(RobotController controller) throws GameActionException{
+		if(!controller.isBuildReady()){
+			return;
+		}
 		int scoutCount = controller.readBroadcast(Constants.BROADCAST_SCOUT_COUNT);
-		if(scoutCount*1000<controller.getTeamBullets()){
+		System.out.println(scoutCount+" - "+(scoutCount*scoutSpawnRate)+" - "+controller.getTeamBullets());
+		if(scoutCount*scoutSpawnRate<controller.getTeamBullets()){
 			int tries = 10;
 			Direction direction = Util.randomDirection();
 			while((!controller.canBuildRobot(RobotType.SCOUT, direction))&&tries>0){
 				direction = Util.randomDirection();
 				tries--;
 			}
-			controller.broadcast(Constants.BROADCAST_SCOUT_COUNT, scoutCount+1);
-			controller.buildRobot(RobotType.SCOUT, direction);
+			if(tries>0){
+				System.out.println("Hiring Scout");
+				controller.broadcast(Constants.BROADCAST_SCOUT_COUNT, scoutCount+1);
+				controller.buildRobot(RobotType.SCOUT, direction);
+				System.out.println("Done Hiring Scout");
+			}
 		}
 	}
 }
--- aninjaz/battlecode/general/ScoutRobot.java
+++ aninjaz/battlecode/general/ScoutRobot.java
@@ -2,22 +2,85 @@ package aninjaz.battlecode.general;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
 
 public class ScoutRobot {
+	public static final int TO_INITIAL_ARCHON_STATE = 0;
+	public static final int RANDOM_DIRECTION_STATE = 1;
+	public static final int TARGET_DIRECTION_STATE = 2;
+	private static int currentState;
+	private static MapLocation targetInitialArchon;
+	private static MapLocation targetLocation;
+	private static Direction direction = Util.randomDirection();
+	private static boolean lowHealth = false;
 	public static void run(RobotController controller) throws GameActionException{
+		targetInitialArchon = getAverage(controller.getInitialArchonLocations(Constants.OTHER_TEAM));
 		while(true){
 			RobotInfo[] nearbyRobots = controller.senseNearbyRobots(-1, Constants.OTHER_TEAM);
-			
 			if(nearbyRobots.length>0){
 				Direction direction = controller.getLocation().directionTo(nearbyRobots[0].getLocation());
 				if(controller.canFireSingleShot()){
 					controller.fireSingleShot(direction);
 				}
+				targetLocation = nearbyRobots[0].getLocation();
+				currentState = TARGET_DIRECTION_STATE;
+			}else{
+				if(currentState==TARGET_DIRECTION_STATE){
+					currentState = RANDOM_DIRECTION_STATE;
+				}
 			}
 			
+			switch(currentState){
+			case TO_INITIAL_ARCHON_STATE:
+				if(!moveTowardsTarget(controller, targetInitialArchon)){
+					while(!controller.canMove(direction)){
+						direction = Util.randomDirection();
+					}
+					controller.move(direction);
+				}
+				if(controller.getLocation().equals(targetInitialArchon)){
+					currentState = RANDOM_DIRECTION_STATE;
+				}
+				break;
+			case RANDOM_DIRECTION_STATE:
+				while(!controller.canMove(direction)){
+					direction = Util.randomDirection();
+				}
+				controller.move(direction);
+				break;
+			case TARGET_DIRECTION_STATE:
+				moveTowardsTarget(controller, targetLocation);
+				break;
+			}
+			if(!lowHealth){
+				if((controller.getHealth()/controller.getType().maxHealth)<Constants.LOW_HEALTH){ //If scout is about to die :(
+					System.out.println("LOW HEALTH: "+controller.readBroadcast(Constants.BROADCAST_SCOUT_COUNT));
+					controller.broadcast(Constants.BROADCAST_SCOUT_COUNT,
+							controller.readBroadcast(Constants.BROADCAST_SCOUT_COUNT)-1);
+					System.out.println("LOW HEALTH 2: "+controller.readBroadcast(Constants.BROADCAST_SCOUT_COUNT));
+					lowHealth = true;
+				}
+			}
 			Util.yieldByteCodes();
 		}
+	}
+	public static boolean moveTowardsTarget(RobotController controller, MapLocation location) throws GameActionException{
+		if(controller.canMove(location)){
+			controller.move(location);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public static MapLocation getAverage(MapLocation[] locations){
+		float totalX = 0;
+		float totalY = 0;
+		for(MapLocation location: locations){
+			totalX+=location.x;
+			totalY+=location.y;
+		}
+		return new MapLocation(totalX/locations.length, totalY/locations.length);
 	}
 }
