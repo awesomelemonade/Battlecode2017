7d7752f5f7d21a56ab95dabb5599b879effd1606
awesomelemonade <awesomelemonade@gmail.com>
2017-01-16 21:32:53 -0500

    Scout Movement AI..

--- aninjaz/battlecode/general/ScoutRobot.java
+++ aninjaz/battlecode/general/ScoutRobot.java
@@ -56,6 +56,16 @@ public class ScoutRobot {
 			if(controller.canMove(directionTowards, distance-Constants.EPSILON)){
 				controller.setIndicatorDot(controller.getLocation(), 0, 255, 0);
 				controller.move(directionTowards, distance-Constants.EPSILON);
+			}else{
+				Direction clockwise = directionTowards.rotateLeftDegrees(45);
+				int tries = 10;
+				while(!controller.canMove(clockwise)&&tries>0){
+					clockwise = directionTowards.rotateLeftDegrees(5);
+					tries--;
+				}
+				if(tries>0){
+					controller.move(clockwise);
+				}
 			}
 			distance = controller.getLocation().distanceTo(bestRobot.getLocation())-2; //Recalculate distance after moving
 			if(distance<=RobotType.SCOUT.bulletSpeed){
