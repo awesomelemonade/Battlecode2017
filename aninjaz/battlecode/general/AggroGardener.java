a9c0e25bdee30c9bfa849afd6e9cc8698c09a2cc
awesomelemonade <awesomelemonade@gmail.com>
2017-01-21 16:40:23 -0500

    Fixed AggroGardener

--- aninjaz/battlecode/general/AggroGardener.java
+++ aninjaz/battlecode/general/AggroGardener.java
@@ -1,11 +1,16 @@
 package aninjaz.battlecode.general;
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 import battlecode.common.RobotType;
 public class AggroGardener {
 	public static void run(RobotController controller) throws GameActionException {
 		while(true){
-			controller.buildRobot(RobotType.SOLDIER, Util.randomDirection());
+			Direction direction = Util.randomDirection();
+			RobotType rand = new RobotType[]{RobotType.SOLDIER, RobotType.SCOUT}[(int)(Math.random()*2)];
+			if(controller.canBuildRobot(RobotType.SOLDIER, direction)){
+				controller.buildRobot(RobotType.SOLDIER, direction);
+			}
 			Util.yieldByteCodes();
 		}
 	}
