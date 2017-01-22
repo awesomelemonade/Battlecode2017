163014204c0730deb87ef01daca384222e7baec7
awesomelemonade <awesomelemonade@gmail.com>
2017-01-21 23:24:31 -0500

    Added Bidirectional Pathfinding

--- aninjaz/battlecode/aggro/AggroSoldier.java
+++ aninjaz/battlecode/aggro/AggroSoldier.java
@@ -3,29 +3,62 @@ package aninjaz.battlecode.aggro;
 import aninjaz.battlecode.general.Util;
 import aninjaz.battlecode.util.Pathfinding;
 import aninjaz.battlecode.general.Constants;
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
 
 public class AggroSoldier {
 	private static MapLocation initialArchon;
+	private static int currentTarget = -1;
+	private static boolean reachedInitialArchon = false;
 	public static void run(RobotController controller) throws GameActionException{
+		Direction direction = Util.randomDirection();
 		initialArchon = controller.getInitialArchonLocations(Constants.OTHER_TEAM)[0];
 		while(true){
 			RobotInfo[] nearbyRobots = controller.senseNearbyRobots(-1, Constants.OTHER_TEAM);
-			if(nearbyRobots.length>0){
-				Pathfinding.goTowards(nearbyRobots[0].getLocation());
+			RobotInfo bestRobot = getBestRobot(nearbyRobots);
+			if(bestRobot!=null){
+				Pathfinding.goTowardsBidirectional(bestRobot.getLocation());
 				if(controller.canFireSingleShot()){
-					controller.fireSingleShot(controller.getLocation().directionTo(nearbyRobots[0].getLocation()));
+					controller.fireSingleShot(controller.getLocation().directionTo(bestRobot.getLocation()));
 				}
 			}else{
-				Pathfinding.goTowards(initialArchon);
-				if(controller.canFireSingleShot()){
-					controller.fireSingleShot(controller.getLocation().directionTo(initialArchon));
+				if(reachedInitialArchon){
+					direction = Util.tryRandomMove(direction);
+				}else{
+					if(Pathfinding.goTowardsBidirectional(initialArchon)==Pathfinding.REACHED_GOAL){
+						reachedInitialArchon = true;
+					}
+					if(controller.getLocation().distanceTo(initialArchon)>controller.getType().sensorRadius){
+						if(controller.canFireSingleShot()){
+							controller.fireSingleShot(controller.getLocation().directionTo(initialArchon));
+						}
+					}
 				}
 			}
 			Util.yieldByteCodes();
 		}
+	}
+	public static RobotInfo getBestRobot(RobotInfo[] robots){
+		if(robots.length==0){
+			return null;
+		}
+		if(currentTarget!=-1){
+			for(RobotInfo robot: robots){
+				if(robot.getID()==currentTarget){
+					return robot;
+				}
+			}
+		}
+		for(RobotInfo robot: robots){
+			if(robot.getType()!=RobotType.ARCHON){
+				currentTarget = robot.getID();
+				return robot;
+			}
+		}
+		return robots[0];
 	}
 }
--- aninjaz/battlecode/util/Pathfinding.java
+++ aninjaz/battlecode/util/Pathfinding.java
@@ -14,6 +14,95 @@ public class Pathfinding {
 	public static final int HAS_MOVED = 1;
 	public static final int HAS_NOT_MOVED = 2;
 	public static RobotController controller;
+	public static int goTowardsBidirectional(MapLocation target) throws GameActionException{
+		float distance = controller.getLocation().distanceTo(target);
+		if(distance==0){
+			return REACHED_GOAL;
+		}
+		Direction direction = controller.getLocation().directionTo(target);
+		controller.setIndicatorDot(target, 0, 255, 0);
+		MapLocation bugPathing = bugPathfindingBidirectional(direction, Math.min(distance, controller.getType().strideRadius), Constants.RANDOM_TRIES);
+		if(controller.canMove(bugPathing)){
+			controller.move(bugPathing);
+			return HAS_MOVED;
+		}else{
+			controller.setIndicatorLine(controller.getLocation(), controller.getLocation().add(controller.getLocation().directionTo(bugPathing), 5f), 0, 0, 0);
+			return HAS_NOT_MOVED;
+		}
+	}
+	public static MapLocation bugPathfindingBidirectional(Direction targetDirection, float targetDistance, int tries) throws GameActionException{
+		MapLocation endpoint = controller.getLocation().add(targetDirection, targetDistance);
+		if(tries<=0){
+			controller.setIndicatorDot(controller.getLocation(), 128, 128, 128);
+			return endpoint;
+		}
+		if(controller.canMove(targetDirection, targetDistance)){
+			return endpoint;
+		}
+		MapLocation startpoint = controller.getLocation();
+		controller.setIndicatorLine(startpoint, endpoint, 255, 128, 0);
+		TreeInfo[] nearbyTrees = controller.senseNearbyTrees(targetDistance+controller.getType().bodyRadius);
+		for(TreeInfo tree: nearbyTrees){
+			MapLocation t = findNearest(tree.getLocation(), startpoint, endpoint);
+			float fatness = controller.getType().bodyRadius+tree.getRadius();
+			float distance = t.distanceTo(tree.getLocation());
+			controller.setIndicatorDot(t, 255, 0, 128);
+			controller.setIndicatorDot(tree.getLocation().add(Util.randomDirection(), (float)(Math.random()*tree.getRadius())), 128, 0, 128);
+			if(distance<fatness){
+				distance = controller.getLocation().distanceTo(tree.getLocation());
+				float angle = (float)Math.asin(fatness/distance);
+				Direction direction = controller.getLocation().directionTo(tree.getLocation());
+				Direction leftDirection = direction.rotateLeftRads(angle);
+				Direction rightDirection = direction.rotateRightRads(angle);
+				float leftBetween = Math.abs(targetDirection.radiansBetween(leftDirection));
+				float rightBetween = Math.abs(targetDirection.radiansBetween(rightDirection));
+				if(leftBetween<rightBetween){
+					if(leftBetween==0){
+						return bugPathfindingBidirectional(leftDirection.rotateLeftRads(Constants.EPSILON), targetDistance, tries-1);
+					}else{
+						return bugPathfindingBidirectional(leftDirection, targetDistance, tries-1);
+					}
+				}else{
+					if(rightBetween==0){
+						return bugPathfindingBidirectional(rightDirection.rotateRightRads(Constants.EPSILON), targetDistance, tries-1);
+					}else{
+						return bugPathfindingBidirectional(rightDirection, targetDistance, tries-1);
+					}
+				}
+			}
+		}
+		RobotInfo[] nearbyRobots = controller.senseNearbyRobots(targetDistance+controller.getType().bodyRadius);
+		for(RobotInfo robot: nearbyRobots){
+			MapLocation t = findNearest(robot.getLocation(), startpoint, endpoint);
+			float fatness = controller.getType().bodyRadius+robot.getRadius();
+			float distance = t.distanceTo(robot.getLocation());
+			controller.setIndicatorDot(t, 255, 0, 128);
+			controller.setIndicatorDot(robot.getLocation().add(Util.randomDirection(), (float)(Math.random()*robot.getRadius())), 128, 0, 128);
+			if(distance<fatness){
+				distance = controller.getLocation().distanceTo(robot.getLocation());
+				float angle = (float)Math.asin(fatness/distance);
+				Direction direction = controller.getLocation().directionTo(robot.getLocation());
+				Direction leftDirection = direction.rotateLeftRads(angle);
+				Direction rightDirection = direction.rotateRightRads(angle);
+				float leftBetween = Math.abs(targetDirection.radiansBetween(leftDirection));
+				float rightBetween = Math.abs(targetDirection.radiansBetween(rightDirection));
+				if(leftBetween<rightBetween){
+					if(leftBetween==0){
+						return bugPathfindingBidirectional(leftDirection.rotateLeftRads(Constants.EPSILON), targetDistance, tries-1);
+					}else{
+						return bugPathfindingBidirectional(leftDirection, targetDistance, tries-1);
+					}
+				}else{
+					if(rightBetween==0){
+						return bugPathfindingBidirectional(rightDirection.rotateRightRads(Constants.EPSILON), targetDistance, tries-1);
+					}else{
+						return bugPathfindingBidirectional(rightDirection, targetDistance, tries-1);
+					}
+				}
+			}
+		}
+		return endpoint; //No Collisions!
+	}
 	public static int goTowards(MapLocation target) throws GameActionException{
 		float distance = controller.getLocation().distanceTo(target);
 		if(distance==0){
