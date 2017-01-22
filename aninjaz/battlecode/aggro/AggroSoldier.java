87bf13cb8ac0aa830d268dededd2e9d4ee3d06b8
awesomelemonade <awesomelemonade@gmail.com>
2017-01-21 22:45:40 -0500

    Fixed AggroSoldier

--- aninjaz/battlecode/aggro/AggroSoldier.java
+++ aninjaz/battlecode/aggro/AggroSoldier.java
@@ -4,24 +4,25 @@ import aninjaz.battlecode.general.Util;
 import aninjaz.battlecode.util.Pathfinding;
 import aninjaz.battlecode.general.Constants;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
 
 public class AggroSoldier {
-	private static RobotInfo closestEnemy = null;
+	private static MapLocation initialArchon;
 	public static void run(RobotController controller) throws GameActionException{
+		initialArchon = controller.getInitialArchonLocations(Constants.OTHER_TEAM)[0];
 		while(true){
-			if(controller.senseNearbyRobots(-1, Constants.OTHER_TEAM)!=null){
-				closestEnemy = controller.senseNearbyRobots(-1, Constants.OTHER_TEAM)[0];
-				Pathfinding.goTowards(controller.getInitialArchonLocations(Constants.OTHER_TEAM)[0]);
-			}
-			else{
-				closestEnemy=null;
-				Pathfinding.goTowards(controller.getInitialArchonLocations(Constants.OTHER_TEAM)[0]);
-			}
-			if(controller.canFireSingleShot()){
-				if(closestEnemy!=null){
-					controller.fireSingleShot(controller.getLocation().directionTo(closestEnemy.getLocation()));
+			RobotInfo[] nearbyRobots = controller.senseNearbyRobots(-1, Constants.OTHER_TEAM);
+			if(nearbyRobots.length>0){
+				Pathfinding.goTowards(nearbyRobots[0].getLocation());
+				if(controller.canFireSingleShot()){
+					controller.fireSingleShot(controller.getLocation().directionTo(nearbyRobots[0].getLocation()));
+				}
+			}else{
+				Pathfinding.goTowards(initialArchon);
+				if(controller.canFireSingleShot()){
+					controller.fireSingleShot(controller.getLocation().directionTo(initialArchon));
 				}
 			}
 			Util.yieldByteCodes();
