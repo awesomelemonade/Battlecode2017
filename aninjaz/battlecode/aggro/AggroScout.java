b3366939f6cdfe3cd2525ca22cdc5508b7ed403a
awesomelemonade <awesomelemonade@gmail.com>
2017-01-22 12:42:43 -0500

    Cleaned up AggroScout; Finalized Aggro Strat

--- aninjaz/battlecode/aggro/AggroScout.java
+++ aninjaz/battlecode/aggro/AggroScout.java
@@ -10,20 +10,17 @@ import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
 
 public class AggroScout {
-	private static RobotController controller;
 	public static void run(RobotController controller) throws GameActionException{
-		AggroScout.controller = controller;
 		Direction direction = Util.randomDirection();
 		while(true){
-			RobotInfo[] nearbyRobots = controller.senseNearbyRobots(-1,Constants.OTHER_TEAM);
+			RobotInfo[] nearbyRobots = controller.senseNearbyRobots(-1, Constants.OTHER_TEAM);
 			RobotInfo nearestGardener = getGardener(nearbyRobots);
 			if(nearestGardener!=null){
 				Pathfinding.goTowardsScout(nearestGardener.getLocation());
 				if(controller.canFireSingleShot()){
 					controller.fireSingleShot(direction);
 				}
-			}
-			else{
+			}else{
 				direction = Util.tryRandomMove(direction);
 			}
 		}
@@ -32,7 +29,6 @@ public class AggroScout {
 		if(robots.length==0){
 			return null;
 		}
-		
 		for(RobotInfo robot: robots){
 			if(robot.getType()==RobotType.GARDENER){
 				return robot;
