8d0376d9a4633279095c63b82cc8d33b6298a3b0
awesomelemonade <awesomelemonade@gmail.com>
2017-01-22 00:17:45 -0500

    Added testupload

---
+++ testupload/RobotPlayer.java
@@ -0,0 +1,9 @@
+package testupload;
+
+import battlecode.common.RobotController;
+
+public class RobotPlayer {
+	public static void run(RobotController controller){
+		RobotPlayer.run(controller);
+	}
+}
