edd4652276cb7984822b63711881289bc52e74cf
awesomelemonade <awesomelemonade@gmail.com>
2017-01-24 01:19:33 -0500

    Fixed Testupload to throw GameActionException

--- testupload/RobotPlayer.java
+++ testupload/RobotPlayer.java
@@ -1,9 +1,10 @@
 package testupload;
 
+import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 
 public class RobotPlayer {
-	public static void run(RobotController controller){
+	public static void run(RobotController controller) throws GameActionException{
 		aninjaz.battlecode.general.RobotPlayer.run(controller);
 	}
 }
